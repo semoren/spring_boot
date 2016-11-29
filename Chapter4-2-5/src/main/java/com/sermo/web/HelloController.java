4d9477fc1b30bb3107eb3d4ac3905c1c302d78ab
sermo <renqing50@gmail.com>
2016-11-29 17:43:01 +0800

    使用log4j记录日志到MongoDB

---
+++ Chapter4-2-5/src/main/java/com/sermo/web/HelloController.java
@@ -0,0 +1,18 @@
+package com.sermo.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author sermo
+ * @version 2016/11/29.
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/hello")
+    public String hello(@RequestParam String name){
+        return "hello " + name;
+    }
+}
---
+++ Chapter4-2-5/src/main/java/com/sermo/web/WebLogAspect.java
@@ -0,0 +1,72 @@
+package com.sermo.web;
+
+import com.mongodb.BasicDBObject;
+import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author sermo
+ * @version 2016/11/29.
+ */
+@Aspect
+@Component
+@Order(1)
+public class WebLogAspect {
+
+    private Logger logger = Logger.getLogger("mongodb");
+
+    @Pointcut("execution(public * com.sermo.web..*.*(..))")
+    public void webLog(){}
+
+    @Before("webLog()")
+    public void doBefore(JoinPoint joinPoint) throws Throwable {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        BasicDBObject logInfo =getBasicDBObject(request, joinPoint);
+        logger.info(logInfo);
+    }
+
+    private BasicDBObject getBasicDBObject(HttpServletRequest request, JoinPoint joinPoint) {
+        BasicDBObject r = new BasicDBObject();
+        r.append("requestURL", request.getRequestURL().toString());
+        r.append("requestURI", request.getRequestURI());
+        r.append("queryString", request.getQueryString());
+        r.append("remoteAddr", request.getRemoteAddr());
+        r.append("remoteHost", request.getRemoteHost());
+        r.append("remotePort", request.getRemotePort());
+        r.append("localAddr", request.getLocalAddr());
+        r.append("localName", request.getLocalName());
+        r.append("method", request.getMethod());
+        r.append("headers", getHeadersInfo(request));
+        r.append("parameters", request.getParameterMap());
+        r.append("classMethod", joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        r.append("args", Arrays.toString(joinPoint.getArgs()));
+        return r;
+    }
+
+
+    private Map<String, String> getHeadersInfo(HttpServletRequest request) {
+        Map<String, String> map = new HashMap<>();
+        Enumeration headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String key = (String) headerNames.nextElement();
+            String value = request.getHeader(key);
+            map.put(key, value);
+        }
+        return map;
+    }
+}
